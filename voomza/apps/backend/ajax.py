969d58796d400400b27669aeba07827cf67a9914
Bryan Cattle <bryan.cattle@gmail.com>
2012-12-12 19:40:07 -0800

    Skeleton of all page classes laid out. Next: build the remaining ones

--- voomza/apps/backend/ajax.py
+++ voomza/apps/backend/ajax.py
@@ -5,7 +5,7 @@ from tastypie.resources import Resource
 from tastypie.authorization import Authorization
 from tastypie.authentication import SessionAuthentication
 from tastypie.api import Api
-from voomza.apps.yearbook.pages import PhotoWithCommentPage, PhotoPage, YearbookPage
+from voomza.apps.yearbook.pages import PhotoWithCommentPage, PhotoPage, YearbookPage, AlbumPage, FieldPage, TopFriendNamePage, BackInTimePage, FriendsCollagePage
 
 
 class PageResource(Resource):
@@ -65,9 +65,11 @@ class PageResource(Resource):
 
 class YearbookPageFactory(object):
     _pages = [
-        PhotoWithCommentPage( page=5,  ranking_name='top_photos',             field_name='top_photo_1'),
-        PhotoPage(            page=6,  ranking_name='top_photos',             field_name='top_photo_2'),
-        PhotoPage(            page=7,  ranking_name='top_photos',             field_name='top_photo_2'),
+        FieldPage(            page=3, field_name='owner.first_name'),
+
+        PhotoPage(            page=5,  ranking_name='top_photos',             field_name='top_photo_1'),
+        PhotoWithCommentPage( page=6,  ranking_name='top_photos',             field_name='top_photo_2'),
+        PhotoWithCommentPage( page=7,  ranking_name='top_photos',             field_name='top_photo_3'),
 
         PhotoPage(            page=9,  ranking_name='top_photos_first_half',  field_name='first_half_photo_1'),
         PhotoPage(            page=10, ranking_name='top_photos_first_half',  field_name='first_half_photo_2'),
@@ -75,6 +77,38 @@ class YearbookPageFactory(object):
         PhotoPage(            page=12, ranking_name='top_photos_second_half', field_name='second_half_photo_1'),
         PhotoPage(            page=13, ranking_name='top_photos_second_half', field_name='second_half_photo_2'),
         PhotoPage(            page=14, ranking_name='top_photos_second_half', field_name='second_half_photo_3'),
+
+        PhotoPage(            page=16, ranking_name='group_shots',            field_name='group_photo_1'),
+        PhotoWithCommentPage( page=17, ranking_name='group_shots',            field_name='group_photo_2'),
+        PhotoWithCommentPage( page=18, ranking_name='group_shots',            field_name='group_photo_3'),
+
+        AlbumPage(            page=20, ranking_name='top_albums.top_album_1', max_photos=4),
+        AlbumPage(            page=21, ranking_name='top_albums.top_album_2', max_photos=4),
+        AlbumPage(            page=22, ranking_name='top_albums.top_album_3', max_photos=4),
+
+        # Top status message
+        FieldPage(            page=26, field_name='top_comment'),
+
+        # Birthday comments
+        FieldPage(            page=27, field_name='birthday_comments'),
+                              # really a two-page spread
+
+        # Top photos back in time
+        BackInTimePage(       page=29),
+                              # really a two-page spread
+
+        TopFriendNamePage(    page=33, ranking_name='top_friends',              field_name='top_friend_1'),
+        PhotoPage(            page=34, ranking_name='top_friends.top_friend_1', field_name='top_friend_1_photo_1'),
+        TopFriendNamePage(    page=35, ranking_name='top_friends',              field_name='top_friend_2'),
+        PhotoPage(            page=36, ranking_name='top_friends.top_friend_2', field_name='top_friend_2_photo_1'),
+        TopFriendNamePage(    page=37, ranking_name='top_friends',              field_name='top_friend_3'),
+        PhotoPage(            page=38, ranking_name='top_friends.top_friend_3', field_name='top_friend_3_photo_1'),
+        TopFriendNamePage(    page=39, ranking_name='top_friends',              field_name='top_friend_4'),
+        PhotoPage(            page=40, ranking_name='top_friends.top_friend_4', field_name='top_friend_4_photo_1'),
+
+        # Friends collage
+        FriendsCollagePage(   page=43),
+                              # really a two-page spread
     ]
 
     @classmethod
--- voomza/apps/yearbook/pages.py
+++ voomza/apps/yearbook/pages.py
@@ -1,5 +1,6 @@
 from voomza.apps.backend.models import PhotoRankings, Yearbook, FacebookPhoto
 
+
 class YearbookPage(object):
     def __init__(self, page):
         self.page = page
@@ -10,7 +11,8 @@ class YearbookPage(object):
         self.yearbook = Yearbook.objects.get(owner=user)
 
     def get_page_content(self, user):
-        raise NotImplementedError
+        self.set_user(user)
+        return self.page_content()
 
     def get_next_content(self, user):
         raise NotImplementedError
@@ -22,8 +24,7 @@ class PhotoPage(YearbookPage):
         self.index_field_name = field_name
         super(PhotoPage, self).__init__(page)
 
-    def get_page_content(self, user):
-        self.set_user(user)
+    def page_content(self):
         # De-reference the field and return pic url
         photo_id = self.yearbook.get_photo_from_field_string(
             self.rankings, self.ranking_table_name, self.index_field_name
@@ -41,15 +42,61 @@ class PhotoPage(YearbookPage):
 
 
 class PhotoWithCommentPage(PhotoPage):
-    @property
     def page_content(self):
         return {
 
         }
 
 
-class FourPhotosPage(PhotoPage):
-    @property
+class TopFriendNamePage(PhotoPage):
+    def page_content(self):
+        # De-reference the field and return the user's first name
+        photo_id = self.yearbook.get_photo_from_field_string(
+            self.rankings, self.ranking_table_name, self.index_field_name
+        )
+        if not photo_id:
+            url = ''
+        else:
+            photo = FacebookPhoto.objects.get(facebook_id=photo_id)
+            url = photo.url()
+
+        return {
+            'url': url,
+            'field': '.'.join([self.ranking_table_name, self.index_field_name]),
+            }
+
+
+class AlbumPage(YearbookPage):
+    def __init__(self, page, ranking_name, max_photos):
+        self.ranking_table_name = ranking_name
+        self.max_photos = max_photos
+        super(AlbumPage, self).__init__(page)
+
+    def page_content(self):
+        return {
+
+        }
+
+
+class FieldPage(YearbookPage):
+    def __init__(self, page, field_name):
+        self.field_name = field_name
+        super(FieldPage, self).__init__(page)
+
+    def page_content(self):
+        return {
+
+        }
+
+
+class BackInTimePage(YearbookPage):
+    def page_content(self):
+        return {
+
+        }
+
+
+class FriendsCollagePage(YearbookPage):
     def page_content(self):
         return {
 
