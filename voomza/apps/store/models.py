13a9164aed519efdd8388e128efd85ce951d31ea
Bryan Cattle <bryan.cattle@gmail.com>
2013-01-08 22:19:34 -0800

    Checkout flow works, still a couple issues - first click doesn't submit - db total charge calc

--- voomza/apps/store/models.py
+++ voomza/apps/store/models.py
@@ -1,9 +1,12 @@
+import logging
 from decimal import Decimal
 from django.db import models
 from django.db.models.signals import post_save
 from django.dispatch.dispatcher import receiver
 from django.contrib.auth.models import User
 from django_countries import CountryField
+
+logger = logging.getLogger(__name__)
 
 
 class StoreProfile(models.Model):
@@ -32,6 +35,7 @@ class Order(models.Model):
     stripe_single_use_token = models.CharField(max_length=255)
     created = models.DateTimeField(auto_now_add=True)
 
+    charged_total = models.CharField(max_length=8, blank=True)
     abridged_quantity = models.PositiveSmallIntegerField(choices=QUANTITY_CHOICES, default=0, blank=True)
     full_quantity = models.PositiveSmallIntegerField(choices=QUANTITY_CHOICES, default=0, blank=True)
     digital_upgrade = models.PositiveSmallIntegerField(choices=DIGITAL_QUANTITY_CHOICES, default=0, blank=True)
@@ -61,7 +65,31 @@ class Order(models.Model):
         tax_subtotal = taxed_subtotal * CA_SALES_TAX if self.shipping_state_province == CA_STATE_ABBR else 0
         return untaxed_subtotal + taxed_subtotal + shipping_subtotal + tax_subtotal
 
+    def populate_from_forms(self, order_form, billing_address_form, shipping_address_form=None):
+        self.stripe_single_use_token = order_form.cleaned_data['stripe_single_use_token']
+        self.abridged_quantity       = order_form.cleaned_data['abridged_quantity']
+        self.full_quantity           = order_form.cleaned_data['full_quantity']
+        self.digital_upgrade         = order_form.cleaned_data['digital_upgrade']
 
+        self.billing_address         = billing_address_form.cleaned_data['billing_address']
+        self.billing_address2        = billing_address_form.cleaned_data['billing_address2']
+        self.billing_country         = billing_address_form.cleaned_data['billing_country']
+        self.billing_city            = billing_address_form.cleaned_data['billing_city']
+        self.billing_state_province  = billing_address_form.cleaned_data['billing_us_state'] if billing_address_form.cleaned_data['billing_country'] == 'US' else billing_address_form.cleaned_data['billing_state_province']
+        self.billing_postal          = billing_address_form.cleaned_data['billing_us_zip'] if billing_address_form.cleaned_data['billing_country'] == 'US' else billing_address_form.cleaned_data['billing_postal']
+
+        if shipping_address_form:
+            self.shipping_name     = shipping_address_form.cleaned_data['shipping_name']
+            self.shipping_address  = shipping_address_form.cleaned_data['shipping_address']
+            self.shipping_address2 = shipping_address_form.cleaned_data['shipping_address2']
+            self.shipping_country  = shipping_address_form.cleaned_data['shipping_country']
+            self.shipping_city     = shipping_address_form.cleaned_data['shipping_city']
+            self.shipping_state_province = shipping_address_form.cleaned_data['shipping_us_state'] if shipping_address_form.cleaned_data['shipping_country'] == 'US' else shipping_address_form.cleaned_data['shipping_state_province']
+            self.shipping_postal   = shipping_address_form.cleaned_data['shipping_us_zip'] if shipping_address_form.cleaned_data['shipping_country'] == 'US' else shipping_address_form.cleaned_data['shipping_postal']
+
+        # Double check the order totals
+#        if 'charged_total' in order_form.cleaned_data and Decimal(order_form.cleaned_data['charged_total']) != self.get_total():
+#            logger.error('OrderForm charged_total didn\'t match model total. %s != %s' % (order_form.cleaned_data['charged_total'], self.get_total()))
 
 
 # Create a StoreProfile when creating a User
--- voomza/apps/store/views.py
+++ voomza/apps/store/views.py
@@ -1,50 +1,78 @@
-from django.shortcuts import render
+import logging
+from django.core.urlresolvers import reverse
+from django.http import HttpResponseRedirect
+from django.shortcuts import render, redirect
+from django.conf import settings
 from voomza.apps.store.forms import OrderForm, OrderBillingAddressForm, \
     OrderShippingAddressForm, CreditCardForm
-from voomza.apps.store.models import PRICE_FOR_FULL, PRICE_FOR_ABRIDGED, PRICE_FOR_DIGITAL, CA_SALES_TAX, PRICE_FOR_SHIPPING
+from voomza.apps.store.models import PRICE_FOR_FULL, PRICE_FOR_ABRIDGED, PRICE_FOR_DIGITAL, CA_SALES_TAX, PRICE_FOR_SHIPPING, Order
+
+logger = logging.getLogger(__name__)
 
 
 def checkout(request, template_name='checkout.html'):
     """
     What the user ordered comes in as a GET param
-    o=full, o=abridged, o=std
+    o=full, o=abridged, else electronic
     """
     order_full, order_abridged, order_std = False, False, False
+    error_message = ''
     if request.method == 'POST':
         order_form = OrderForm(request.POST)
         billing_address_form = OrderBillingAddressForm(request.POST)
-        shipping_address_form = OrderShippingAddressForm(request.POST)
-        credit_card_form = CreditCardForm(request.POST)
-        pass
-    else:
-        if 'o' in request.GET:
-            if request.GET['o'] == 'full':
-                order_full = True
-            elif request.GET['o'] == 'abridged':
-                order_abridged = True
-            else:
-                order_std = True
-        else:
-            order_std = True
+        shipping_address_form = None
+        if 'same_shipping' in request.POST and request.POST['same_shipping'] == 'diff':
+            shipping_address_form = OrderShippingAddressForm(request.POST)
 
-        order_form = OrderForm()
-        billing_address_form = OrderBillingAddressForm()
-        shipping_address_form = OrderShippingAddressForm()
-        credit_card_form = CreditCardForm()
+        # If any forms fail to validate, log an error and return an error message
+        error = False
+        if not order_form.is_valid():
+            logger.error('OrderForm failed to validate: %s. POST: %s' % (order_form.errors, request.POST))
+            error = True
+        if not billing_address_form.is_valid():
+            import ipdb
+            ipdb.set_trace()
+
+            logger.error('OrderBillingAddressForm failed to validate: %s. POST: %s' % (billing_address_form.errors, request.POST))
+            error = True
+        if shipping_address_form and not shipping_address_form.is_valid():
+            logger.error('OrderShippingAddressForm failed to validate: %s. POST: %s' % (shipping_address_form.errors, request.POST))
+            error = True
+
+        if not error:
+            # Save model
+            order = Order(user=request.user)
+            order.populate_from_forms(order_form, billing_address_form, shipping_address_form)
+            order.save()
+            # Return redirect to homepage
+            return HttpResponseRedirect(reverse('homepage') + '?c=order')
+        else:
+            # Set error message
+            error_message = u'Sorry, an error occurred. Please try again.'
+            # Fall thorugh to checkout page
+
+    if 'o' in request.GET:
+        if request.GET['o'] == 'full':
+            order_full = True
+        elif request.GET['o'] == 'abridged':
+            order_abridged = True
+
     context = {
-        'order_form':               order_form,
-        'billing_address_form':     billing_address_form,
-        'shipping_address_form':    shipping_address_form,
-        'credit_card_form':         credit_card_form,
+        'error_message':            error_message,
+        'order_form':               OrderForm(),
+        'billing_address_form':     OrderBillingAddressForm(),
+        'shipping_address_form':    OrderShippingAddressForm(),
+        'credit_card_form':         CreditCardForm(),
 
         'order_full':       order_full,
         'order_abridged':   order_abridged,
-        'order_std':        order_std,
 
         'full_price':       PRICE_FOR_FULL,
         'abridged_price':   PRICE_FOR_ABRIDGED,
         'shipping_price':   PRICE_FOR_SHIPPING,
         'digital_price':    PRICE_FOR_DIGITAL,
         'ca_tax_rate':      CA_SALES_TAX,
+
+        'STRIPE_PUBLISHABLE_KEY': settings.STRIPE_PUBLISHABLE_KEY,
     }
     return render(request, template_name, context)
