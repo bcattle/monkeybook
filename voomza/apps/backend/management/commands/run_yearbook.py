7e4bbf7da30eb6fa66d4d7f2137597c1f8db098a
Bryan Cattle <bryan.cattle@gmail.com>
2013-02-19 11:49:47 -0800

    Added `run_yearbook` command

---
+++ voomza/apps/backend/management/commands/run_yearbook.py
@@ -0,0 +1,21 @@
+from django.core.management.base import BaseCommand
+from django.contrib.auth.models import User
+from backend.tasks import top_friends_fast
+
+
+class Command(BaseCommand):
+    args = '<username>'
+    help = 'Runs the specified user\'s yearbook. Leaves the original in place'
+
+    def handle(self, *args, **options):
+        username = args[0]
+
+        try:
+            user = User.objects.get(username=username)
+        except User.DoesNotExist:
+            print 'No user found with username <%s>' % username
+            return
+
+        # Run `top_friends_fast` with `run_yearbook`=True
+        top_friends_fast.apply_async(kwargs={'user': user, 'run_yearbook': True})
+        print 'Enqueued yearbook for username <%s>' % username
